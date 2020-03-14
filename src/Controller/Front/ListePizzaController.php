0635796bd3a6485f97de42da1086a31db6da702b
1nacim <nacim.moulaoui@gmail.com>
2020-03-14 21:56:15 +0000

    [Shopping Cart] - Fin jQuery front panier RAF : Configurer cache pour ajouter les pizzas mises dans le panier

--- src/Controller/Front/ListePizzaController.php
+++ src/Controller/Front/ListePizzaController.php
@@ -2,12 +2,20 @@
 
 namespace App\Controller\Front;
 
-use App\Entity\Pizza;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
+/**
+ * Class ListePizzaController
+ * @package App\Controller\Front
+ */
 class ListePizzaController extends AbstractController
 {
+    /**
+     * @return Response
+     */
     public function index()
     {
         $em = $this->getDoctrine()->getManager();
@@ -20,5 +28,20 @@ class ListePizzaController extends AbstractController
                 'pizzas' => $pizzas
             ]
         );
+    }
+
+    /**
+     * @param Request $request
+     * @return Response|null
+     */
+    public function ajouterPizzaAuPanier(Request $request)
+    {
+        if ($request->isMethod('POST')) {
+            $em = $this->getDoctrine()->getManager();
+            $pizzaRepository = $em->getRepository('App:Pizza');
+            $pizzaRepository->find($request->get('id'));
+        }
+
+        return new Response(null, 400);
     }
 }
